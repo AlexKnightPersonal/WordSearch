25ef7640890cb6b02a6bdd1467fd101f24ce5644
AKTheknight <alex@aktheknight.co.uk>
2016-10-26 11:51:16 +0100

    Add project files.

---
+++ WordSearch/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WordSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGo);
+            this.groupBox1.Controls.Add(this.txtSize);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(210, 490);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Grid Settings";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(210, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(580, 490);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(7, 20);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSize.TabIndex = 0;
+            this.txtSize.Text = "6";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(113, 18);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 490);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WordSearch/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+        private int size;
+        private readonly Random rand;
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //Get size of Word Search
+            size = Convert.ToInt32(txtSize.Text);
+
+            //For debug
+            //size = 6;
+
+            //Set size of Word Search
+            dataGridView1.RowCount = dataGridView1.ColumnCount = size;
+            //Ensure grid is refreshed every time
+            EmptyGrid();
+
+
+            WriteUp("alex");
+        }
+
+        private void WriteDown(string word)
+        {
+            //Get column and starting row
+            int column = rand.Next(size);
+            int startRow = rand.Next(size);
+            //Ensure word doesn't overflow grid
+            if ((startRow + word.Length) > size)
+                startRow = (startRow- word.Length) < 0 ? startRow - (size - word.Length) : startRow -= word.Length;
+
+            //Write word
+            var step = 0;
+            foreach (var c in word.ToCharArray())
+            {
+                dataGridView1.Rows[startRow + step].Cells[column].Value = c;
+                step++;
+            }
+        }
+
+        private void WriteUp(string word)
+        {
+            //Get column and starting row
+            int column = rand.Next(size);
+            int startRow = rand.Next(size);
+            //Ensure word doesn't overflow grid
+            if ((startRow - word.Length) < 0)
+                startRow = startRow + word.Length > size ? startRow + (word.Length - startRow) : startRow += word.Length - 1;
+
+            //Write word
+            var step = 0;
+            foreach (var c in word.ToCharArray())
+            {
+                dataGridView1.Rows[startRow - step].Cells[column].Value = c;
+                step++;
+            }
+        }
+
+
+
+        private void EmptyGrid()
+        {
+            foreach (var cell in from DataGridViewRow row in dataGridView1.Rows
+                                 from DataGridViewCell cell in row.Cells
+                                 select cell)
+            {
+                cell.Value = string.Empty;
+            }
+        }
+    }
+}
